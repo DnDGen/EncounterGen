3beec191bfbb2f7ba264da4cc781abf5cef7ddb1
Karl Speer <kspeer@qualtrax.com>
2015-11-16 16:49:55 -0500

    Getting solution set up

---
+++ Tests/Unit/Generators/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterGen.Tests.Unit.Generators
+{
+    public class Class1
+    {
+    }
+}
