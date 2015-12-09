1bb9871d5b8b4404a516a5e333f0ddbdf085e4dd
Karl Speer <cidthecoatrack@gmail.com>
2015-12-08 23:01:23 -0500

    more tables

--- EncounterGen/Common/CreatureConstants.cs
+++ EncounterGen/Common/CreatureConstants.cs
@@ -4,9 +4,14 @@ namespace EncounterGen.Common
 {
     public static class CreatureConstants
     {
+        public const String Allip = "Allip";
+        public const String Ankheg = "Ankheg";
         public const String Ant_Giant = "Giant ant";
-        public const String AstralDeva = "Astral deva (angel)";
+        public const String AstralDeva = "Astral deva (celestial)";
         public const String Balor = "Balor (demon)";
+        public const String Boar_Dire = "Dire boar";
+        public const String Bugbear = "Bugbear";
+        public const String CarrionCrawler = "Carrion crawler";
         public const String Centipede_Monstrous_Large = "Large monstrous centipede";
         public const String Centipede_Monstrous_Medium = "Medium monstrous centipede";
         public const String Character = "Character";
@@ -17,31 +22,55 @@ namespace EncounterGen.Common
         public const String DwarfWarrior = "Dwarf warrior";
         public const String ElfWarrior = "Elf warrior";
         public const String EtherealMarauder = "Ethereal marauder";
+        public const String Ettercap = "Ettercap";
         public const String FireBeetle_Giant = "Giant fire beetle";
+        public const String FormianWarrior = "Formian warrior";
         public const String FormianWorker = "Formian worker";
         public const String Gelugon = "Gelugon (devil)";
+        public const String Ghost = "Ghost";
         public const String Ghoul = "Ghoul";
         public const String Glabrezu = "Glabrezu (demon)";
+        public const String Gnoll = "Gnoll";
         public const String Goblin = "Goblin";
         public const String Hobgoblin = "Hobgoblin";
+        public const String Hyena = "Hyena";
+        public const String Imp = "Imp (devil)";
         public const String Kobold = "Kobold";
         public const String Krenshar = "Krenshar";
+        public const String LanternArchon = "Lantern archon (celestial)";
         public const String Lemure = "Lemure (devil)";
+        public const String Lizard_Giant = "Giant lizard";
         public const String Lizardfolk = "Lizardfolk";
+        public const String Magmin = "Magmin";
+        public const String Marilith = "Marilith (demon)";
+        public const String Mimic = "Mimic";
+        public const String Nalfeshnee = "Nalfeshnee (demon)";
+        public const String Nightcrawler = "Nightcrawler (nightshade)";
         public const String Nightwalker = "Nightwalker (nightshade)";
+        public const String Ogre = "Ogre";
+        public const String Ooze_Gray = "Gray ooze";
         public const String Orc = "Orc";
         public const String PitFiend = "Pit fiend (devil)";
-        public const String Planetar = "Planetar (angel)";
+        public const String Planetar = "Planetar (celestial)";
+        public const String Quasit = "Quasit (demon)";
         public const String Rat_Dire = "Dire rat";
+        public const String RustMonster = "Rust monster";
         public const String Scorpion_Monstrous_Medium = "Medium monstrous scorpion";
         public const String Scorpion_Monstrous_Small = "Small monstrous scorpion";
         public const String Shrieker = "Shrieker";
         public const String Skeleton_HumanWarrior = "Human warrior skeleton";
+        public const String Snake_Viper_Huge = "Huge viper snake";
         public const String Snake_Viper_Tiny = "Tiny viper snake";
+        public const String Solar = "Solar (celestial)";
         public const String Spider_Monstrous_Medium = "Medium monstrous spider";
         public const String Spider_Monstrous_Small = "Small monstrous spider";
         public const String Stirge = "Stirge";
         public const String Vampire = "Vampire";
+        public const String VampireSpawn = "Vampire spawn";
+        public const String VioletFungi = "Violet fungi";
+        public const String Weasel_Dire = "Dire weasel";
+        public const String Wight = "Wight";
+        public const String Worg = "Worg";
         public const String Zombie_HumanCommoner = "Human commoner zombie";
     }
 }
---
+++ Tests/Integration/Tables/Encounters/Dungeon/Level19DungeonEncountersTests.cs
@@ -0,0 +1,48 @@
+﻿using EncounterGen.Common;
+using EncounterGen.Selectors;
+using EncounterGen.Tables;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterGen.Tests.Integration.Tables.Encounters.Dungeon
+{
+    [TestFixture]
+    public class Level19DungeonEncountersTests : TypeAndAmountPercentileTests
+    {
+        protected override String tableName
+        {
+            get
+            {
+                return String.Format(TableNameConstants.LevelXENVIRONMENTEncounters, 19, EnvironmentConstants.Dungeon);
+            }
+        }
+
+        [Test]
+        public override void TableIsComplete()
+        {
+            AssertTableIsComplete();
+        }
+
+        [TestCase(1, 20, CreatureConstants.Dragon, RollConstants.One)]
+        [TestCase(31, 40, CreatureConstants.PitFiend, RollConstants.OneD3Plus1)]
+        [TestCase(41, 45, CreatureConstants.Solar, RollConstants.OneD3)]
+        [TestCase(46, 70, CreatureConstants.Character, RollConstants.OneD3Plus1)]
+        [TestCase(71, 80, CreatureConstants.Nalfeshnee, RollConstants.OneD3Plus1)]
+        [TestCase(81, 90, CreatureConstants.Ghost, RollConstants.One)]
+        [TestCase(91, 100, CreatureConstants.Nightcrawler, RollConstants.OneD3)]
+        public override void Percentile(Int32 lower, Int32 upper, String type, String amount)
+        {
+            base.Percentile(lower, upper, type, amount);
+        }
+
+        [TestCase(21, 30, CreatureConstants.Marilith, RollConstants.One, CreatureConstants.Glabrezu, RollConstants.OneD3)]
+        public override void Percentile(Int32 lower, Int32 upper, String firstType, String firstAmount, String secondType, String secondAmount)
+        {
+            base.Percentile(lower, upper, firstType, firstAmount, secondType, secondAmount);
+        }
+    }
+}
