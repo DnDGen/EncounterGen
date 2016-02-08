44360a8d3cb6b52f9c9619d93a61ed0f1c32ec5b
Karl Speer <karl.speer@icfi.com>
2016-02-08 14:52:03 -0500

    Update for formiam taskmaster
    Excel tables have info to include dominated creatures
    
    * Need to sort and adjust excel tables
    * need to sort and adjust created tables
    * Need to build vehicle for creating dominated creatures in the
    encounter generator.  Should be just a "creature"' with a random subtype
    that is all CR4 creatures

--- Tests/Integration/Tables/Encounters/Dungeon/Level7DungeonEncountersTests.cs
+++ Tests/Integration/Tables/Encounters/Dungeon/Level7DungeonEncountersTests.cs
@@ -22,41 +22,34 @@ namespace EncounterGen.Tests.Integration.Tables.Encounters.Dungeon
             AssertTableIsComplete();
         }
 
-        [TestCase(1, 2, CreatureConstants.BlackPudding, RollConstants.One)]
-        [TestCase(3, 5, CreatureConstants.Centipede_Monstrous_Gargantuan, RollConstants.OneD2)]
-        [TestCase(6, 9, CreatureConstants.Ooze_OchreJelly, RollConstants.One)]
-        [TestCase(10, 11, CreatureConstants.Snake_Constrictor_Giant, RollConstants.One)]
-        [TestCase(12, 12, CreatureConstants.Spider_Monstrous_Huge, RollConstants.OneD2)]
-        [TestCase(13, 15, CreatureConstants.SpiderEater, RollConstants.One)]
+        [TestCase(1, 4, CreatureConstants.BlackPudding, RollConstants.One)]
+        [TestCase(5, 5, CreatureConstants.Centipede_Monstrous_Gargantuan, RollConstants.OneD2)]
+        [TestCase(6, 8, CreatureConstants.Criosphinx, RollConstants.One)]
+        [TestCase(9, 10, CreatureConstants.Boar_Dire, RollConstants.OneD3Plus1)]
+        [TestCase(11, 14, CreatureConstants.Remorhaz, RollConstants.One)]
+        [TestCase(15, 15, CreatureConstants.Scorpion_Monstrous_Huge, RollConstants.OneD2)]
         [TestCase(16, 20, CreatureConstants.Dragon, RollConstants.One)]
-        [TestCase(21, 23, CreatureConstants.Doppelganger, RollConstants.OneD3)]
-        [TestCase(24, 25, CreatureConstants.GreenHag, RollConstants.One)]
-        [TestCase(26, 27, CreatureConstants.Mephit, RollConstants.OneD3)]
-        [TestCase(28, 30, CreatureConstants.Wererat, RollConstants.OneD3Plus1)]
-        [TestCase(31, 35, CreatureConstants.BlinkDog, RollConstants.OneD3Plus1)]
-        [TestCase(36, 40, CreatureConstants.Character + "[2]", RollConstants.OneD3Plus1)]
-        [TestCase(41, 43, CreatureConstants.Cockatrice, RollConstants.OneD3)]
-        [TestCase(44, 47, CreatureConstants.GibberingMouther, RollConstants.One)]
-        [TestCase(48, 50, CreatureConstants.Grick, RollConstants.OneD3)]
-        [TestCase(51, 52, CreatureConstants.Hydra + " (1d3+4 heads)", RollConstants.One)]
-        [TestCase(53, 55, CreatureConstants.Nightmare, RollConstants.One)]
-        [TestCase(56, 58, CreatureConstants.ShockerLizard, RollConstants.OneD3Plus1)]
-        [TestCase(59, 60, CreatureConstants.VioletFungus, RollConstants.One,
-            CreatureConstants.Shrieker, RollConstants.OneD3Plus1)]
-        [TestCase(61, 64, CreatureConstants.Azer, RollConstants.OneD3Plus1)]
-        [TestCase(65, 67, CreatureConstants.Bugbear, RollConstants.OneD3Plus1)]
-        [TestCase(68, 69, CreatureConstants.Ettercap, RollConstants.One,
-            CreatureConstants.Spider_Monstrous_Medium, RollConstants.OneD3)]
-        [TestCase(70, 72, CreatureConstants.Ogre, RollConstants.OneD3Plus1)]
-        [TestCase(73, 75, CreatureConstants.Salamander_Flamebrother, RollConstants.OneD3Plus1)]
-        [TestCase(76, 77, CreatureConstants.Troglodyte, RollConstants.OneD3Plus1,
-            CreatureConstants.Lizard_Monitor, RollConstants.OneD2)]
-        [TestCase(78, 80, CreatureConstants.Worg, RollConstants.OneD3Plus1)]
-        [TestCase(81, 85, CreatureConstants.Ghast, RollConstants.One,
-            CreatureConstants.Ghoul, RollConstants.OneD3Plus1)]
-        [TestCase(86, 90, CreatureConstants.Mummy, RollConstants.OneD3)]
-        [TestCase(91, 95, CreatureConstants.Skeleton_HillGiant, RollConstants.OneD3Plus1)]
-        [TestCase(96, 100, CreatureConstants.Wraith, RollConstants.One)]
+        [TestCase(21, 22, CreatureConstants.Aranea, RollConstants.OneD3Plus1)]
+        [TestCase(23, 24, CreatureConstants.Barghest, RollConstants.OneD3Plus1)]
+        [TestCase(25, 26, CreatureConstants.Djinn, RollConstants.OneD3Plus1)]
+        [TestCase(27, 28, CreatureConstants.FormianTaskmaster, RollConstants.One,
+            CreatureConstants.Minotaur, RollConstants.One)]
+        [TestCase(29, 30, CreatureConstants.Janni, RollConstants.OneD3Plus1)]
+        [TestCase(31, 35, CreatureConstants.HoundArchon, RollConstants.OneD3Plus1)]
+        [TestCase(36, 40, CreatureConstants.Character + "[4]", RollConstants.OneD3Plus1)]
+        [TestCase(41, 45, CreatureConstants.Cloaker, RollConstants.OneD3)]
+        [TestCase(46, 48, CreatureConstants.Cryohydra + " (1d3+4 heads)", RollConstants.One)]
+        [TestCase(49, 52, CreatureConstants.FormianWarrior, RollConstants.OneD4Plus2)]
+        [TestCase(53, 57, CreatureConstants.InvisibleStalker, RollConstants.One)]
+        [TestCase(58, 60, CreatureConstants.Pyrohydra + " (1d3+4 heads)", RollConstants.One)]
+        [TestCase(61, 65, CreatureConstants.Bugbear, RollConstants.OneD3Plus1,
+            CreatureConstants.Wolf, RollConstants.OneD3Plus1)]
+        [TestCase(66, 70, CreatureConstants.Ettin, RollConstants.One,
+            CreatureConstants.Bear_Brown, RollConstants.OneD2)]
+        [TestCase(71, 75, CreatureConstants.Minotaur, RollConstants.OneD3Plus1)]
+        [TestCase(76, 80, CreatureConstants.Salamander_Average, RollConstants.OneD3Plus1)]
+        [TestCase(81, 90, CreatureConstants.Ghost + "[1d3+3]", RollConstants.One)]
+        [TestCase(91, 100, CreatureConstants.Vampire + "[1d2+4]", RollConstants.One)]
         public override void Percentile(int lower, int upper, params string[] typesAndAmounts)
         {
             base.Percentile(lower, upper, typesAndAmounts);
