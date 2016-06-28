5c690994b9f61c9688f9c76ac93811d98c98a235
Karl Speer <cidthecoatrack@gmail.com>
2016-06-28 16:58:27 -0400

    And yet more tables
    * Only a few civilized encounter tables left
    * Updated Nunit

---
+++ EncounterGen.Tests.Integration.Tables/Encounters/Civilized/Day/Level15CivilizedDayEncountersTests.cs
@@ -0,0 +1,59 @@
+﻿using CharacterGen.CharacterClasses;
+using EncounterGen.Common;
+using EncounterGen.Domain.Selectors;
+using EncounterGen.Domain.Tables;
+using NUnit.Framework;
+
+namespace EncounterGen.Tests.Integration.Tables.Encounters.Civilized.Day
+{
+    [TestFixture]
+    public class Level15CivilizedDayEncountersTests : TypeAndAmountPercentileTests
+    {
+        protected override string tableName
+        {
+            get
+            {
+                return string.Format(TableNameConstants.LevelXENVIRONMENTEncounters, 15, EnvironmentConstants.CivilizedDay);
+            }
+        }
+
+        [Test]
+        public override void TableIsComplete()
+        {
+            AssertTableIsComplete();
+        }
+
+        [TestCase(1, 6, CreatureConstants.Commoner_Merchant + "[20]", RollConstants.OneD3,
+            CreatureConstants.Warrior_Guard + "[20]", RollConstants.OneD3,
+            CreatureConstants.Expert_Merchant + "[20]", RollConstants.One)]
+        [TestCase(7, 12, CreatureConstants.Commoner_Minstrel + "[1d2+17]", RollConstants.OneD6Plus3,
+            CreatureConstants.Expert_Minstrel + "[1d2+17]", RollConstants.One)]
+        [TestCase(13, 18, CreatureConstants.Commoner_Minstrel + "[1d2+17]", RollConstants.OneD6Plus3,
+            CharacterClassConstants.Bard + "[11]", RollConstants.One)]
+        [TestCase(19, 24, CreatureConstants.Warrior_Patrol + "[1d2+17]", RollConstants.OneD6Plus3,
+            CharacterClassConstants.Fighter + "[11]", RollConstants.One)]
+        [TestCase(25, 30, CreatureConstants.Commoner_Pilgrim + "[1d2+11]", RollConstants.OneD4Plus10,
+            CreatureConstants.Warrior_Guard + "[1d2+11]", RollConstants.OneD3Plus1,
+            CharacterClassConstants.Cleric + "[12]", RollConstants.One)]
+        [TestCase(31, 36, CreatureConstants.Commoner_Pilgrim + "[1d2+15]", RollConstants.OneD6Plus5,
+            CreatureConstants.Warrior_Guard + "[1d2+15]", RollConstants.OneD3,
+            CharacterClassConstants.Cleric + "[10]", RollConstants.One)]
+        [TestCase(37, 42, CreatureConstants.Commoner_Pilgrim + "[1d2+17]", RollConstants.OneD6Plus3,
+            CharacterClassConstants.Cleric + "[11]", RollConstants.One)]
+        [TestCase(43, 48, CreatureConstants.Warrior_Bandit + "[1d2+17]", RollConstants.OneD6Plus3,
+            CreatureConstants.Warrior_BanditLeader + "[20]", RollConstants.One)]
+        [TestCase(49, 54, CreatureConstants.Warrior_Bandit + "[1d2+15]", RollConstants.OneD6Plus5,
+            CharacterClassConstants.Fighter + "[10]", RollConstants.One)]
+        [TestCase(55, 60, CreatureConstants.Warrior_Bandit + "[1d2+13]", RollConstants.OneD4Plus10,
+            CharacterClassConstants.Fighter + "[10]", RollConstants.One)]
+        [TestCase(61, 68, CreatureConstants.Character + "[15]", RollConstants.One)]
+        [TestCase(69, 76, CreatureConstants.Character + "[14]", RollConstants.OneD2)]
+        [TestCase(77, 84, CreatureConstants.Character + "[13]", RollConstants.OneD3)]
+        [TestCase(85, 92, CreatureConstants.Character + "[12]", RollConstants.OneD3Plus1)]
+        [TestCase(93, 100, CreatureConstants.Character + "[11]", RollConstants.OneD4Plus2)]
+        public override void Percentile(int lower, int upper, params string[] typesAndAmounts)
+        {
+            base.Percentile(lower, upper, typesAndAmounts);
+        }
+    }
+}
