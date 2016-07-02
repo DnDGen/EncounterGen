1008f59d060a29f0b4a654e2715a6004e729d1b9
Karl Speer <cidthecoatrack@gmail.com>
2016-07-02 14:07:10 -0400

    Finish tests
    Now just to get them passing

---
+++ EncounterGen.Tests.Integration.Tables/Encounters/Civilized/Day/Level18CivilizedDayEnountersTests.cs
@@ -0,0 +1,41 @@
+﻿using CharacterGen.CharacterClasses;
+using EncounterGen.Common;
+using EncounterGen.Domain.Selectors;
+using EncounterGen.Domain.Tables;
+using NUnit.Framework;
+
+namespace EncounterGen.Tests.Integration.Tables.Encounters.Civilized.Day
+{
+    [TestFixture]
+    public class Level18CivilizedDayEnountersTests : TypeAndAmountPercentileTests
+    {
+        protected override string tableName
+        {
+            get
+            {
+                return string.Format(TableNameConstants.LevelXENVIRONMENTEncounters, 18, EnvironmentConstants.CivilizedDay);
+            }
+        }
+
+        [Test]
+        public override void TableIsComplete()
+        {
+            AssertTableIsComplete();
+        }
+
+        [TestCase(1, 14, CreatureConstants.Commoner_Pilgrim + "[1d2+17]", RollConstants.OneD4Plus10,
+            CreatureConstants.Warrior_Guard + "[1d2+17]", RollConstants.OneD3Plus1,
+            CharacterClassConstants.Cleric + "[15]", RollConstants.One)]
+        [TestCase(15, 28, CreatureConstants.Warrior_Bandit + "[20]", RollConstants.OneD4Plus10,
+            CharacterClassConstants.Fighter + "[13]", RollConstants.One)]
+        [TestCase(29, 42, CreatureConstants.Character + "[18]", RollConstants.One)]
+        [TestCase(43, 56, CreatureConstants.Character + "[17]", RollConstants.OneD2)]
+        [TestCase(57, 70, CreatureConstants.Character + "[16]", RollConstants.OneD3)]
+        [TestCase(71, 84, CreatureConstants.Character + "[15]", RollConstants.OneD3Plus1)]
+        [TestCase(85, 100, CreatureConstants.Character + "[14]", RollConstants.OneD4Plus2)]
+        public override void Percentile(int lower, int upper, params string[] typesAndAmounts)
+        {
+            base.Percentile(lower, upper, typesAndAmounts);
+        }
+    }
+}
---
+++ EncounterGen.Tests.Integration.Tables/Encounters/Civilized/Night/Level18CivilizedNightEncountersTests.cs
@@ -0,0 +1,43 @@
+﻿using CharacterGen.CharacterClasses;
+using EncounterGen.Common;
+using EncounterGen.Domain.Selectors;
+using EncounterGen.Domain.Tables;
+using NUnit.Framework;
+
+namespace EncounterGen.Tests.Integration.Tables.Encounters.Civilized.Night
+{
+    [TestFixture]
+    public class Level18CivilizedNightEncountersTests : TypeAndAmountPercentileTests
+    {
+        protected override string tableName
+        {
+            get
+            {
+                return string.Format(TableNameConstants.LevelXENVIRONMENTEncounters, 18, EnvironmentConstants.CivilizedNight);
+            }
+        }
+
+        [Test]
+        public override void TableIsComplete()
+        {
+            AssertTableIsComplete();
+        }
+
+        [TestCase(1, 11, CreatureConstants.Ghost + "[13]", RollConstants.One)]
+        [TestCase(12, 22, CreatureConstants.Commoner_Pilgrim + "[1d2+17]", RollConstants.OneD4Plus10,
+            CreatureConstants.Warrior_Guard + "[1d2+17]", RollConstants.OneD3Plus1,
+            CharacterClassConstants.Cleric + "[15]", RollConstants.One)]
+        [TestCase(23, 33, CreatureConstants.Warrior_Bandit + "[20]", RollConstants.OneD4Plus10,
+            CharacterClassConstants.Fighter + "[13]", RollConstants.One)]
+        [TestCase(34, 44, CreatureConstants.Vampire + "[10]", RollConstants.One)]
+        [TestCase(45, 55, CreatureConstants.Character + "[18]", RollConstants.One)]
+        [TestCase(56, 66, CreatureConstants.Character + "[17]", RollConstants.OneD2)]
+        [TestCase(67, 77, CreatureConstants.Character + "[16]", RollConstants.OneD3)]
+        [TestCase(78, 88, CreatureConstants.Character + "[15]", RollConstants.OneD3Plus1)]
+        [TestCase(89, 100, CreatureConstants.Character + "[14]", RollConstants.OneD4Plus2)]
+        public override void Percentile(int lower, int upper, params string[] typesAndAmounts)
+        {
+            base.Percentile(lower, upper, typesAndAmounts);
+        }
+    }
+}
