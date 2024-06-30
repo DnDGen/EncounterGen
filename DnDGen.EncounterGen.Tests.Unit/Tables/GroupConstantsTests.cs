78ca36a7adf8217f8325fcfb5252c1c998d49883
Karl Speer <cidthecoatrack@gmail.com>
2024-06-29 22:02:58 -0400

    Rebalance character encounters
    They are occurring too often. Unless we are in a civilized environment, character encounters should be uncommon.

--- DnDGen.EncounterGen.Tests.Unit/Tables/GroupConstantsTests.cs
+++ DnDGen.EncounterGen.Tests.Unit/Tables/GroupConstantsTests.cs
@@ -6,10 +6,12 @@ namespace DnDGen.EncounterGen.Tests.Unit.Tables
     [TestFixture]
     public class GroupConstantsTests
     {
+        [TestCase(GroupConstants.All, "All")]
+        [TestCase(GroupConstants.Character, "Character")]
+        [TestCase(GroupConstants.Extraplanar, "Extraplanar")]
         [TestCase(GroupConstants.RequiresSubcreature, "Requires Sub-creature")]
         [TestCase(GroupConstants.UseSubcreatureForTreasure, "Use Sub-creature for Treasure")]
         [TestCase(GroupConstants.Wilderness, "Wilderness")]
-        [TestCase(GroupConstants.Extraplanar, "Extraplanar")]
         public void Constant(string constant, string value)
         {
             Assert.That(constant, Is.EqualTo(value));
--- DnDGen.EncounterGen/Tables/GroupConstants.cs
+++ DnDGen.EncounterGen/Tables/GroupConstants.cs
@@ -2,10 +2,11 @@
 {
     internal static class GroupConstants
     {
+        public const string All = "All";
+        public const string Character = "Character";
+        public const string Extraplanar = "Extraplanar";
         public const string RequiresSubcreature = "Requires Sub-creature";
         public const string UseSubcreatureForTreasure = "Use Sub-creature for Treasure";
         public const string Wilderness = "Wilderness";
-        public const string Extraplanar = "Extraplanar";
-        public const string All = "All";
     }
 }
